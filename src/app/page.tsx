bc76dbbdd649afaa9a69f51b0525eaaed12d624f
beezital <63583702+beezital@users.noreply.github.com>
2025-08-20 17:39:23 +0200

    add history to game

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,11 +13,38 @@ function Square({ value, onSquareClick }: { value: string | null, onSquareClick:
   );
 }
 
-export default function Board() {
+function Board({ squares, onSquareClick }: { squares: Array<string | null>, onSquareClick: (i: number) => void }) {
+
+
+  return (
+    <>
+      <div className="board-row">
+        <Square value={squares[0]} onSquareClick={onSquareClick.bind(null, 0)} />
+        <Square value={squares[1]} onSquareClick={onSquareClick.bind(null, 1)} />
+        <Square value={squares[2]} onSquareClick={onSquareClick.bind(null, 2)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[3]} onSquareClick={() => onSquareClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => onSquareClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => onSquareClick(5)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[6]} onSquareClick={() => onSquareClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => onSquareClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => onSquareClick(8)} />
+      </div>
+    </>
+  );
+}
+
+
+export default function Game() {
 
   const [winner, setWinner] = useState<string | null>(null);
   const [nextPlayer, setNextPlayer] = useState<string>('X');
-  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [history, setHistory] = useState<Array<Array<string | null>>>([Array(9).fill(null)]);
+
+  const squares = history[history.length - 1];
 
   function onSquareClick(i: number) {
     if (squares[i] || winner) {
@@ -25,11 +52,18 @@ export default function Board() {
     }
     const newSquares = squares.slice();
     newSquares[i] = nextPlayer;
-    setSquares(newSquares);
-    setNextPlayer(nextPlayer === 'X' ? 'O' : 'X');
+
+    const newHistory = [...history, newSquares];
+    console.log(newHistory);
+    setHistory(newHistory);
+    togglePlayer();
 
     const newWinner = calculateWinner(newSquares);
     setWinner(newWinner);
+  }
+
+  function togglePlayer() {
+    setNextPlayer(nextPlayer === 'X' ? 'O' : 'X');
   }
 
   function calculateWinner(squares: Array<string | null>) {
@@ -59,24 +93,23 @@ export default function Board() {
     return <p>Next player: {nextPlayer}</p>;
   }
 
+  function cancelLastMove() {
+    if (history.length <= 1) {
+      return; // No moves to cancel
+    }
+    const newHistory = history.slice(0, history.length - 1);
+
+    setHistory(newHistory);
+    togglePlayer();
+  }
+
   return (
     <>
       {player(nextPlayer, winner)}
-      <div className="board-row">
-        <Square value={squares[0]} onSquareClick={onSquareClick.bind(null, 0)} />
-        <Square value={squares[1]} onSquareClick={onSquareClick.bind(null, 1)} />
-        <Square value={squares[2]} onSquareClick={onSquareClick.bind(null, 2)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => onSquareClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => onSquareClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => onSquareClick(5)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => onSquareClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => onSquareClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => onSquareClick(8)} />
-      </div>
+      <Board squares={squares} onSquareClick={onSquareClick} />
+      <p>
+        <button onClick={cancelLastMove} disabled={ history.length <= 1}>Cancel Last Move</button>
+      </p>
     </>
   );
 }
