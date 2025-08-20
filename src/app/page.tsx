2be488f87f63ba0f0394bba45ed87f875b55dbec
beezital <63583702+beezital@users.noreply.github.com>
2025-08-20 15:06:44 +0200

    move state to board

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,41 +2,43 @@
 import { useState } from 'react';
 
 
-function Square() {
-
-  const [ value, setValue ] = useState<string | null>(null);
-
-  function handleClick() {
-    console.log('click');
-    setValue('X');
-  }
+function Square({ value, onSquareClick } : { value: string | null, onSquareClick: () => void }) {
 
   return (
     <button
       className="square"
-      onClick={handleClick}>
+      onClick={onSquareClick}>
       {value}
     </button>
   );
 }
 
 export default function Board() {
+
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function onSquareClick(i: number) {
+    const newSquares = squares.slice();
+    newSquares[i] = 'X';
+    setSquares(newSquares);
+  }
+
   return (
     <>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[0]} onSquareClick={onSquareClick.bind(null, 0)} />
+        <Square value={squares[1]} onSquareClick={onSquareClick.bind(null, 1)} />
+        <Square value={squares[2]} onSquareClick={onSquareClick.bind(null, 2)} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[3]} onSquareClick={() => onSquareClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => onSquareClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => onSquareClick(5)} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[6]} onSquareClick={() => onSquareClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => onSquareClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => onSquareClick(8)} />
       </div>
     </>
   );
