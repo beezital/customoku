e2488c80416ae45ada50e51ada799bffd980d018
beezital <63583702+beezital@users.noreply.github.com>
2025-08-24 12:36:37 +0200

    Model + onToggle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,6 +16,32 @@ type HistoryEntry = {
   nextPlayer: string;
 };
 
+// Define the Mode type as  an enum
+enum Mode {
+  Init = 'init',
+  Solve = 'solve',
+  Mark = 'mark',
+}
+
+// Define Cell model
+type CellModel = {
+  cellId: number;
+  value?: number | undefined;
+  isLocked: boolean;
+  marks: Array<boolean>; // Array of 9 booleans indicating possible marks (1-9)
+};
+
+// Define Grid model
+type GridModel = {
+  gridId: number;
+  cells: Array<CellModel>;
+};
+
+// Define Board model
+type BoardModel = {
+  grids: Array<GridModel>;
+};
+
 
 // Mark: display the given value or nothing (null) in a square area.
 // Holds the hints the user gives to the parent Cell
@@ -25,53 +51,65 @@ function Mark({ value }: { value?: number | undefined }) {
   )
 }
 
+function MarkGrid({ cellModel }: { cellModel: CellModel }) {
+  return <div style={{ position: "absolute", color: "#000A" }}>
+    <div style={{ display: "flex" }}>
+      <Mark value={cellModel.marks[0] ? 1 : undefined} />
+      <Mark value={cellModel.marks[1] ? 2 : undefined} />
+      <Mark value={cellModel.marks[2] ? 3 : undefined} />
+    </div>
+    <div style={{ display: "flex" }}>
+      <Mark value={cellModel.marks[3] ? 4 : undefined} />
+      <Mark value={cellModel.marks[4] ? 5 : undefined} />
+      <Mark value={cellModel.marks[5] ? 6 : undefined} />
+    </div>
+    <div style={{ display: "flex" }}>
+      <Mark value={cellModel.marks[6] ? 7 : undefined} />
+      <Mark value={cellModel.marks[7] ? 8 : undefined} />
+      <Mark value={cellModel.marks[8] ? 9 : undefined} />
+    </div>
+  </div>;
+}
+
 // Cell: constituant of a Grid of 3x3 Cells
 // Displays the given value as set by the original Sudoku puzzle or as set by the user
-function Cell({ value }: { value?: number | undefined }) {
+function Cell({ cellModel, gridId, cellId, onToggle }: { cellModel: CellModel, gridId: number, cellId: number, onToggle: (gridId: number, cellId: number) => void }) {
+
   return (
-    <div style={{ position: "relative", display: "inline-block", width: "3.9em", aspectRatio: "1", border: "1px solid gray" }} >
+    <div
+      style={{ position: "relative", display: "inline-block", width: "3.9em", aspectRatio: "1", border: "1px solid gray" }}
+      onClick={() => onToggle(gridId, cellId)}
+    >
       <div style={{ position: "absolute", width: "3.9em", aspectRatio: "1", display: "flex", alignItems: "center", justifyContent: "center", background: "transparent" }}>
-        <span style={{ fontSize: "3em", fontWeight: "normal", color: "#00F8" }}>{value}</span>
+        <span style={{ fontSize: "3em", fontWeight: "normal", color: (cellModel.isLocked ? "#000" : "#00F8") }}>{cellModel.value}</span>
       </div>
-      <div style={{ position: "absolute", color: "#000A" }}>
-        <div style={{ display: "flex" }}>
-          <Mark value={1} />
-          <Mark value={2} />
-          <Mark value={3} />
-        </div>
-        <div style={{ display: "flex" }}>
-          <Mark value={4} />
-          <Mark value={5} />
-          <Mark value={6} />
-        </div>
-        <div style={{ display: "flex" }}>
-          <Mark value={7} />
-          <Mark value={8} />
-          <Mark value={9} />
-        </div>
-      </div>
+      {
+        !cellModel.isLocked && (
+          <MarkGrid cellModel={cellModel} />
+        )
+      }
     </div>
   )
 }
 
-function Grid() {
+function Grid({ gridModel, gridId, onToggle }: { gridModel: GridModel, gridId: number, onToggle: (gridId: number, cellId: number) => void }) {
   return (
     <>
       <div style={{ display: "inline-block", border: "2px solid black" }}>
         <div style={{ display: "flex" }}>
-          <Cell value={1} />
-          <Cell value={2} />
-          <Cell value={3} />
+          <Cell cellModel={gridModel.cells[0]} gridId={gridId} cellId={0} onToggle={onToggle} />
+          <Cell cellModel={gridModel.cells[1]} gridId={gridId} cellId={1} onToggle={onToggle} />
+          <Cell cellModel={gridModel.cells[2]} gridId={gridId} cellId={2} onToggle={onToggle} />
         </div>
         <div style={{ display: "flex" }}>
-          <Cell value={4} />
-          <Cell value={5} />
-          <Cell value={6} />
+          <Cell cellModel={gridModel.cells[3]} gridId={gridId} cellId={3} onToggle={onToggle} />
+          <Cell cellModel={gridModel.cells[4]} gridId={gridId} cellId={4} onToggle={onToggle} />
+          <Cell cellModel={gridModel.cells[5]} gridId={gridId} cellId={5} onToggle={onToggle} />
         </div>
         <div style={{ display: "flex" }}>
-          <Cell value={7} />
-          <Cell value={8} />
-          <Cell value={9} />
+          <Cell cellModel={gridModel.cells[6]} gridId={gridId} cellId={6} onToggle={onToggle} />
+          <Cell cellModel={gridModel.cells[7]} gridId={gridId} cellId={7} onToggle={onToggle} />
+          <Cell cellModel={gridModel.cells[8]} gridId={gridId} cellId={8} onToggle={onToggle} />
         </div>
       </div>
     </>
@@ -79,23 +117,23 @@ function Grid() {
 }
 
 // Board: grid of 3x3 Grid components
-function Board() {
+function Board({ boardModel, onToggle }: { boardModel: BoardModel, onToggle: (gridId: number, cellId: number) => void }) {
   return (
     <div>
       <div style={{ display: "flex" }}>
-        <Grid />
-        <Grid />
-        <Grid />
+        <Grid gridModel={boardModel.grids[0]} gridId={0} onToggle={onToggle} />
+        <Grid gridModel={boardModel.grids[1]} gridId={1} onToggle={onToggle} />
+        <Grid gridModel={boardModel.grids[2]} gridId={2} onToggle={onToggle} />
       </div>
       <div style={{ display: "flex" }}>
-        <Grid />
-        <Grid />
-        <Grid />
+        <Grid gridModel={boardModel.grids[3]} gridId={3} onToggle={onToggle} />
+        <Grid gridModel={boardModel.grids[4]} gridId={4} onToggle={onToggle} />
+        <Grid gridModel={boardModel.grids[5]} gridId={5} onToggle={onToggle} />
       </div>
       <div style={{ display: "flex" }}>
-        <Grid />
-        <Grid />
-        <Grid />
+        <Grid gridModel={boardModel.grids[6]} gridId={6} onToggle={onToggle} />
+        <Grid gridModel={boardModel.grids[7]} gridId={7} onToggle={onToggle} />
+        <Grid gridModel={boardModel.grids[8]} gridId={8} onToggle={onToggle} />
       </div>
     </div>
   );
@@ -141,9 +179,24 @@ export default function Game() {
 
   const [winner, setWinner] = useState<string | null>(null);
   const [history, setHistory] = useState<HistoryEntry[]>([{ squares: Array(9).fill(null), nextPlayer: 'X' }]);
-  
+
   // mode state
-  const [mode, setMode] = useState<'init' | 'solve' | 'mark'>('init');
+  const [mode, setMode] = useState<Mode>(Mode.Init);
+  // digit state
+  const [digit, setDigit] = useState<number | undefined>(undefined);
+
+  // initialise the board model
+  const [boardModel, setBoardModel] = useState<BoardModel>({
+    grids: Array.from({ length: 9 }, (_, gridId) => ({
+      gridId,
+      cells: Array.from({ length: 9 }, (_, cellId) => ({
+        cellId,
+        value: undefined,
+        isLocked: false,
+        marks: Array(9).fill(false)
+      }))
+    }))
+  });
 
   const squares = history[history.length - 1].squares;
   const nextPlayer = history[history.length - 1].nextPlayer;
@@ -208,22 +261,66 @@ export default function Game() {
     checkAndUpdateWinner(newHistory);
   }
 
+  function onToggleCell(): (gridId: number, cellId: number) => void {
+    return (gridId, cellId) => {
+      if (digit === undefined) {
+        return;
+      }
+      const newBoardModel = { ...boardModel };
+      const cell = newBoardModel.grids[gridId].cells[cellId];
+      switch (mode) {
+        case Mode.Init:
+        case Mode.Solve:
+          cell.value = cell.value === undefined ? digit : undefined;
+          if (mode === Mode.Init) {
+            cell.isLocked = cell.value !== undefined;
+          }
+          break;
+        case Mode.Mark:
+          cell.marks[digit-1] = !cell.marks[digit-1];
+          break;
+      }
+      setBoardModel(newBoardModel);
+    };
+  }
+
   return (
     <div style={{ display: "flex", flexWrap: "wrap", flexDirection: "row", alignItems: "center", gap: "1em", padding: "1em" }}>
-      <Board />
-      <ToggleButtonGroup
-        color="primary"
-        value={mode}
-        exclusive
-        aria-label="Platform"
-        onChange={(event, newMode) => {
-          setMode(newMode);
-        }}
-      >
-        <ToggleButton value="init">Init</ToggleButton>
-        <ToggleButton value="solve">Solve</ToggleButton>
-        <ToggleButton value="mark">Mark</ToggleButton>
-      </ToggleButtonGroup>
+      <Board boardModel={boardModel} onToggle={onToggleCell()} />
+      <div style={{ display: "flex", flexDirection: "column", alignItems: "center", gap: "1em" }}>
+        <ToggleButtonGroup
+          color="primary"
+          value={mode}
+          exclusive
+          aria-label="Mode"
+          onChange={(event, newMode) => {
+            setMode(newMode);
+          }}
+        >
+          <ToggleButton value={Mode.Init}>Init</ToggleButton>
+          <ToggleButton value={Mode.Solve}>Solve</ToggleButton>
+          <ToggleButton value={Mode.Mark}>Mark</ToggleButton>
+        </ToggleButtonGroup>
+        <ToggleButtonGroup
+          color="primary"
+          value={digit}
+          exclusive
+          aria-label="Digit"
+          onChange={(event, newDigit) => {
+            setDigit(newDigit);
+          }}
+        >
+          <ToggleButton value={1}>1</ToggleButton>
+          <ToggleButton value={2}>2</ToggleButton>
+          <ToggleButton value={3}>3</ToggleButton>
+          <ToggleButton value={4}>4</ToggleButton>
+          <ToggleButton value={5}>5</ToggleButton>
+          <ToggleButton value={6}>6</ToggleButton>
+          <ToggleButton value={7}>7</ToggleButton>
+          <ToggleButton value={8}>8</ToggleButton>
+          <ToggleButton value={9}>9</ToggleButton>
+        </ToggleButtonGroup>
+      </div>
     </div>
   );
 }
