700ffe9fae1f1e82378e4c86a3428f5de2f2555f
beezital <63583702+beezital@users.noreply.github.com>
2025-08-24 12:39:24 +0200

    cleanup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,13 +9,6 @@ import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
 
 import { useState } from 'react';
 
-// type for the history of the board state
-// includes the array of squares and the next player for each move
-type HistoryEntry = {
-  squares: Array<string | null>;
-  nextPlayer: string;
-};
-
 // Define the Mode type as  an enum
 enum Mode {
   Init = 'init',
@@ -139,46 +132,8 @@ function Board({ boardModel, onToggle }: { boardModel: BoardModel, onToggle: (gr
   );
 }
 
-function Square({ value, onSquareClick }: { value: string | null, onSquareClick: () => void }) {
-
-  return (
-    <button
-      className="square"
-      onClick={onSquareClick}>
-      {value}
-    </button>
-  );
-}
-
-function XBoard({ squares, onSquareClick }: { squares: Array<string | null>, onSquareClick: (i: number) => void }) {
-
-
-  return (
-    <>
-      <div className="board-row">
-        <Square value={squares[0]} onSquareClick={onSquareClick.bind(null, 0)} />
-        <Square value={squares[1]} onSquareClick={onSquareClick.bind(null, 1)} />
-        <Square value={squares[2]} onSquareClick={onSquareClick.bind(null, 2)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => onSquareClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => onSquareClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => onSquareClick(5)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => onSquareClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => onSquareClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => onSquareClick(8)} />
-      </div>
-    </>
-  );
-}
-
 
 export default function Game() {
-
-  const [winner, setWinner] = useState<string | null>(null);
-  const [history, setHistory] = useState<HistoryEntry[]>([{ squares: Array(9).fill(null), nextPlayer: 'X' }]);
 
   // mode state
   const [mode, setMode] = useState<Mode>(Mode.Init);
@@ -198,68 +153,6 @@ export default function Game() {
     }))
   });
 
-  const squares = history[history.length - 1].squares;
-  const nextPlayer = history[history.length - 1].nextPlayer;
-
-  function onSquareClick(i: number) {
-    if (squares[i] || winner) {
-      return; // Ignore if the square is already filled
-    }
-
-    const newSquares = squares.slice();
-    newSquares[i] = nextPlayer;
-    const newNextPlayer = nextPlayer === 'X' ? 'O' : 'X';
-    const newHistoryEntry: HistoryEntry = { squares: newSquares, nextPlayer: newNextPlayer };
-
-    const newHistory = [...history, newHistoryEntry];
-    console.log(newHistory);
-    setHistory(newHistory);
-
-    checkAndUpdateWinner(newHistory);
-  }
-
-  function checkAndUpdateWinner(history: HistoryEntry[]) {
-    const squares = history[history.length - 1].squares;
-    const newWinner = calculateWinner(squares);
-    setWinner(newWinner);
-  }
-
-  function calculateWinner(squares: Array<string | null>) {
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6]
-    ];
-    for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
-      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-        return squares[a];
-      }
-    }
-    return null;
-  }
-
-  function player(nextPlayer: string, winner: string | null) {
-    if (winner) {
-      return <h1>Winner: {winner}</h1>;
-    }
-    return <p>Next player: {nextPlayer}</p>;
-  }
-
-  function cancelLastMove() {
-    if (history.length <= 1) {
-      return; // No moves to cancel
-    }
-    const newHistory = history.slice(0, history.length - 1);
-
-    setHistory(newHistory);
-    checkAndUpdateWinner(newHistory);
-  }
 
   function onToggleCell(): (gridId: number, cellId: number) => void {
     return (gridId, cellId) => {
